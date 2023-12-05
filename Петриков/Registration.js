628473b0933ea9ed0a731c3d793afbaf6abbac1f
Artem Petrikov <112977031+ManForTen@users.noreply.github.com>
2023-12-05 21:54:37 +0300

    Петриков Артём

---
+++ Петриков/Registration.js
@@ -0,0 +1,17 @@
+export default function Registration() {
+
+
+    return (
+        <>
+            <p className={"AddStudent"}>Регистрация</p>
+            <input placeholder={"Логин"}/>
+            <br/>
+            <br/>
+            <input placeholder={"Пароль"}/>
+            <br/>
+            <br/>
+            <button>Зарегистрироваться</button>
+
+        </>
+    );
+}
